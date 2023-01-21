19abd0fc66f39c945c23a3d6b2d968fdf3706db9
Jibaru <ignacioruedaboada@hotmail.com>
2023-01-21 12:35:21 -0500

    style: apply psr-12 style

---
+++ tests/ValueObjects/InternalSaleTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace ValueObjects;
+
+use Jibaru\NubefactApi\ValueObjects\SunatTransactions\InternalSale;
+use PHPUnit\Framework\TestCase;
+
+class InternalSaleTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function testNewInstance()
+    {
+        $expectedName = 'VENTA INTERNA';
+        $expectedValue = 1;
+
+        $instance = new InternalSale();
+
+        $name = $instance->name();
+        $value = $instance->value();
+
+        $this->assertSame($expectedName, $name);
+        $this->assertSame($expectedValue, $value);
+    }
+}
